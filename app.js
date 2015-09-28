9dbbb87bdf3e11295bc302222043c55a2208e27c
Don Schemmel <dons500@gmail.com>
2015-09-27 21:05:12 -0700

    Make required changes

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 var courseRoster = angular.module('courseRoster', ['ui.router']);
 
-courseRoster.config(function($stateProvider, $urlRouterProvider) {
+courseRoster.config(function($stateProvider) {
+
   $stateProvider.state('home', {
-    url: "",
-    templateUrl: "partials/home.html"
+    url: '',
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
+  });
+
+  $stateProvider.state('courses', {
+    url: "/courses",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/courses.html",
+        controller: 'CoursesCtrl'
+      },
+    }
+  });
+
+  $stateProvider.state('courses.students', {
+    url: "/:courseId",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/courses.students.html",
+        controller: 'StudentsCtrl'
+      },
+    }
   });
 });
